508c0390ed67e34d679a2a450c4a3d90ec1bcde5
terrence-ou <outerrencedl@gmail.com>
2024-05-24 22:03:00 -0400

    event.js documentation updated

--- packages/runtime/src/event.js
+++ packages/runtime/src/event.js
@@ -1,8 +1,10 @@
-export function addEventListener(eventName, handler, el) {
-  el.addEventListener(eventName, handler);
-  return handler;
-}
-
+/**
+ * Add event listeners to the event target el, and returns the added listeners
+ *
+ * @param {object} listeners
+ * @param {EventTarget} el
+ * @returns added event listeners
+ */
 export function addEventListeners(listeners = {}, el) {
   const addedListeners = {};
   Object.entries(listeners).forEach(([eventName, handler]) => {
@@ -10,4 +12,17 @@ export function addEventListeners(listeners = {}, el) {
     addEventListener[eventName] = listener;
   });
   return addedListeners;
+}
+
+/**
+ * Attachs an event listener to the event target, and returns the event handler
+ *
+ * @param {string} eventName the name of the event
+ * @param {(event:Event) => void} handler the event callback function
+ * @param {EventTarget} el the element to add event listener to
+ * @returns the event handler
+ */
+export function addEventListener(eventName, handler, el) {
+  el.addEventListener(eventName, handler);
+  return handler;
 }
