166b6dbcf3090e57a32cf78b5fb5dff303bb2013
terrence-ou <outerrencedl@gmail.com>
2024-06-17 15:29:45 -0400

    rollup plugins updated

--- packages/runtime/rollup.config.mjs
+++ packages/runtime/rollup.config.mjs
@@ -1,10 +1,10 @@
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import cleanup from "rollup-plugin-cleanup";
 import filesize from "rollup-plugin-filesize";
 
 export default {
   input: "src/index.js",
-  plugins: [cleanup()],
-  output: [
-    { file: "dist/moyu.js", format: "esm", plugins: [filesize()] },
-  ],
+  plugins: [cleanup(), commonjs(), nodeResolve()],
+  output: [{ file: "dist/moyu.js", format: "esm", plugins: [filesize()] }],
 };
