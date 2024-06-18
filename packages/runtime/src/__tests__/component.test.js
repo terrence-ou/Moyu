1fbcb3882bff576c4ea4d0d08bf9a8fddae5a11f
terrence-ou <outerrencedl@gmail.com>
2024-06-17 21:51:45 -0400

    test cases updated

---
+++ packages/runtime/src/__tests__/component.test.js
@@ -0,0 +1,125 @@
+import { describe, it, beforeEach, expect } from "vitest";
+import { defineComponent } from "../component";
+import { h, hFragment } from "../h";
+
+beforeEach(() => {
+  document.body.innerHTML = "";
+});
+
+describe("Testing mounting and unmounting component", () => {
+  it("mount place holder component", () => {
+    const comp = new PlaceHolderComponent();
+    comp.mount(document.body);
+    expect(document.body.innerHTML).toBe("<p>A place holder component</p>");
+  });
+
+  it("mounting twice throws an error", () => {
+    const comp = new PlaceHolderComponent();
+    comp.mount(document.body);
+    expect(() => comp.mount(document.body)).toThrowError(
+      "Component is already mounted",
+    );
+  });
+
+  it("mount to specific index", () => {
+    document.body.innerHTML = "<h1>Testing</h1>";
+    const comp = new PlaceHolderComponent();
+    comp.mount(document.body, 1);
+    expect(document.body.innerHTML).toBe(
+      "<h1>Testing</h1><p>A place holder component</p>",
+    );
+    comp.unmount();
+    comp.mount(document.body, 0);
+    expect(document.body.innerHTML).toBe(
+      "<p>A place holder component</p><h1>Testing</h1>",
+    );
+  });
+
+  it("unmound place holder component", () => {
+    const comp = new PlaceHolderComponent();
+    comp.mount(document.body);
+    comp.unmount();
+    expect(document.body.innerHTML).toEqual("");
+  });
+
+  it("mount after unmount", () => {
+    const comp = new PlaceHolderComponent();
+    comp.mount(document.body);
+    comp.unmount();
+    comp.mount(document.body);
+    expect(document.body.innerHTML).toEqual("<p>A place holder component</p>");
+  });
+
+  it("mount fragments", () => {
+    const comp = new FragmentComponent();
+    comp.mount(document.body);
+    expect(document.body.innerHTML).toEqual(
+      "<p>Placeholder</p><p>And another</p>",
+    );
+  });
+});
+
+describe("Testing components with props", () => {
+  it("classname", () => {
+    const comp = new ComponentWithProps({ className: "placeholder" });
+    comp.mount(document.body);
+    expect(document.body.innerHTML).includes('class="placeholder"');
+  });
+
+  it("classname and id", () => {
+    const comp = new ComponentWithProps({
+      id: "id-holder",
+      className: "class-holder",
+    });
+    comp.mount(document.body);
+    expect(document.body.innerHTML).includes('class="class-holder"');
+    expect(document.body.innerHTML).includes('id="id-holder"');
+  });
+});
+
+describe("Testing getter - elements", () => {
+  it("get elements before mounting", () => {
+    const comp = new PlaceHolderComponent();
+    expect(comp.elements).toBeInstanceOf(Array);
+    expect(comp.elements.length).toBe(0);
+  });
+
+  it("get elements after mounting", () => {
+    const comp = new PlaceHolderComponent();
+    comp.mount(document.body);
+    expect(comp.elements).toBeInstanceOf(Array);
+    expect(comp.firstElement).toBeInstanceOf(HTMLParagraphElement);
+  });
+
+  it("get fragment component elements", () => {
+    const comp = new FragmentComponent();
+    comp.mount(document.body);
+    expect(comp.elements.length).toBe(2);
+    comp.elements.forEach((el) => {
+      expect(el).toBeInstanceOf(HTMLParagraphElement);
+    });
+  });
+});
+
+const PlaceHolderComponent = defineComponent({
+  render() {
+    return h("p", {}, ["A place holder component"]);
+  },
+});
+
+const FragmentComponent = defineComponent({
+  render() {
+    return hFragment([
+      h("p", {}, ["Placeholder"]),
+      h("p", {}, ["And another"]),
+    ]);
+  },
+});
+
+const ComponentWithProps = defineComponent({
+  render() {
+    return h("p", { class: this.props.className, id: this.props.id }, [
+      "Placeholder",
+    ]);
+  },
+});
--- packages/runtime/src/__tests__/objects.test.js
+++ packages/runtime/src/__tests__/objects.test.js
@@ -1,38 +1,53 @@
-import { it, expect } from "vitest";
-import { objectsDiff } from "../utils/objects";
+import { it, expect, describe } from "vitest";
+import { objectsDiff, hasOwnProperty } from "../utils/objects";
 
-it("same object, no changes were made", () => {
-  const oldObj = { foo: "bar" };
-  const newObj = { foo: "bar" };
-  const { added, removed, updated } = objectsDiff(oldObj, newObj);
-  expect(added).toEqual([]);
-  expect(removed).toEqual([]);
-  expect(updated).toEqual([]);
+describe("Testing objectsDiff function", () => {
+  it("same object, no changes were made", () => {
+    const oldObj = { foo: "bar" };
+    const newObj = { foo: "bar" };
+    const { added, removed, updated } = objectsDiff(oldObj, newObj);
+    expect(added).toEqual([]);
+    expect(removed).toEqual([]);
+    expect(updated).toEqual([]);
+  });
+
+  it("key added", () => {
+    const oldObj = { foo: "bar" };
+    const newObj = { foo: "bar", far: "har" };
+    const { added, removed, updated } = objectsDiff(oldObj, newObj);
+    expect(added).toEqual(["far"]);
+    expect(removed).toEqual([]);
+    expect(updated).toEqual([]);
+  });
+
+  it("key removed", () => {
+    const oldObj = { foo: "bar" };
+    const newObj = {};
+    const { added, removed, updated } = objectsDiff(oldObj, newObj);
+    expect(added).toEqual([]);
+    expect(removed).toEqual(["foo"]);
+    expect(updated).toEqual([]);
+  });
+
+  it("key value updated", () => {
+    const oldObj = { foo: "bar", far: "har" };
+    const newObj = { foo: "har", far: "bar" };
+    const { added, removed, updated } = objectsDiff(oldObj, newObj);
+    expect(added).toEqual([]);
+    expect(removed).toEqual([]);
+    expect(updated).toEqual(["foo", "far"]);
+  });
 });
 
-it("key added", () => {
-  const oldObj = { foo: "bar" };
-  const newObj = { foo: "bar", far: "har" };
-  const { added, removed, updated } = objectsDiff(oldObj, newObj);
-  expect(added).toEqual(["far"]);
-  expect(removed).toEqual([]);
-  expect(updated).toEqual([]);
-});
-
-it("key removed", () => {
-  const oldObj = { foo: "bar" };
-  const newObj = {};
-  const { added, removed, updated } = objectsDiff(oldObj, newObj);
-  expect(added).toEqual([]);
-  expect(removed).toEqual(["foo"]);
-  expect(updated).toEqual([]);
-});
-
-it("key value updated", () => {
-  const oldObj = { foo: "bar", far: "har" };
-  const newObj = { foo: "har", far: "bar" };
-  const { added, removed, updated } = objectsDiff(oldObj, newObj);
-  expect(added).toEqual([]);
-  expect(removed).toEqual([]);
-  expect(updated).toEqual(["foo", "far"]);
+describe("Testing hasOwnProperty function", () => {
+  it("The object has the given property", () => {
+    const obj = { foo: "bar", far: "har" };
+    expect(hasOwnProperty(obj, "foo")).toBe(true);
+    expect(hasOwnProperty(obj, "far")).toBe(true);
+  });
+  it("The object does not have the given property", () => {
+    const obj = {};
+    expect(hasOwnProperty(obj, "foo")).toBe(false);
+    expect(hasOwnProperty(obj, "far")).toBe(false);
+  });
 });
